1c687d0ccc3deddc445157af84e6bd5ed74f8c31
Märt <lhmus88@gmail.com>
2013-12-12 11:00:58 +0200

    Update gcode.c

--- gcode.c
+++ gcode.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-float gcode(float x, float y, float w, float h, float z, float Z){
-        printf("G X GoTo %f\n", x);
-        printf("G Y GoTo %f\n", y);
-        printf("G Z Down %f\n", z);
-        printf("G X Move %f\n", w);
+void koodid (float x, float y, float w, float h){
+        printf("G X GoTo %f\n", x );
+        printf("G Y GoTo %f\n", y );
+        printf("G Z Down\n");
+        printf("G X Move %f\n", w );
         printf("G Y Move %f\n", h);
-        printf("G X Move -%f\n", w);
-        printf("G Y Move -%f\n", h);
-        printf("G Z Up %f\n", Z);
+        printf("G X Move %f\n", -w);
+        printf("G Y Move %f\n", -h);
+        printf("G Z Up\n");
+        
 }
 
-int main(int argc, char *argv[]){
-        gcode(100, 200, 44, 33, 0.5, 20);
+int main(int argc, char * argv[]) {
+        koodid(100, 200, 30, 44);
+        
         return 0;
 }
